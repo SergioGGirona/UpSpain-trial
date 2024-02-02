f4287322fd0d52aaeda83317dba2f8da745280e6
Sergio G. Girona <sergioglez_8@hotmail.com>
2024-02-02 11:05:51 +0100

    Add header and cards

--- projects/rick-api/src/app/app.component.ts
+++ projects/rick-api/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardsComponent } from './components/cards/cards.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'trial-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CardsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'rickApi';
+  title = 'Rick & Morty';
 }
